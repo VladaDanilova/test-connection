4d7aeba90ba662c186e0aa8208df33bd76e68253
Vlada <48159402+VladaDanilova@users.noreply.github.com>
2024-03-19 11:05:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='test-connection',
+    name='MIGRATION TEST - link',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A simple project description',
     long_description=open('README.md').read(),
-    install_requires=['numpy', 'tensorflow'],  # Add tensorflow here
+    install_requires=['numpy', 'tensorflow'], 
     url='https://github.com/VladaDanilova/test-connection',
     author='Vlada Danilova',
     author_email='just.you177@gmail.com'
